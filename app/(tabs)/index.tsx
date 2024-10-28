4dd5eef55fa071d3e67227da27b066b99c0d9005
CazinSacha <cazin.lep@gmail.com>
2024-10-28 09:40:01 +0100

    changement titre appli

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,7 @@ export default function TabOneScreen() {
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Text style={styles.title}>Raconte ton rêve</Text>
+        <Text style={styles.title}>Dream Journal</Text>
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
         <DreamForm />
       </View>
